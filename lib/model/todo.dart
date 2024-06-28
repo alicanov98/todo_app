40e064e06b0b6a86f9ed1004a12f6003a0ce4762
alicanov98 <alicanov1998@gmail.com>
2024-06-29 02:01:00 +0400

    edit

---
+++ lib/model/todo.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Todo {
+  Todo({
+    required this.id,
+    required this.todo,
+    required this.completed,
+    required this.userID,
+  });
+  int? id;
+  String? todo;
+  bool? completed;
+  int? userID;
+
+  // FROM JSON
+  Todo.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    todo = json['todo'];
+    completed = json['completed'];
+    userID = json['userID'];
+  }
+  // TO JSON
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+    data['id'] = id;
+    data['todo'] = todo;
+    data['completed'] = completed;
+    data['userID'] = userID;
+    return data;
+  }
+}
